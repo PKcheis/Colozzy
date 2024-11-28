896b6a9eab8eb625a156a58a339965ea1725bf6c
Pkcheis <philipkubaichege@gmail.com>
2024-11-28 07:11:50 +0300

    Screen resolution adjustable

--- school/app/layout.tsx
+++ school/app/layout.tsx
@@ -1,7 +1,6 @@
 // In your layout.tsx or page.tsx
 "use client";
 
-import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { useEffect, useState } from "react";
